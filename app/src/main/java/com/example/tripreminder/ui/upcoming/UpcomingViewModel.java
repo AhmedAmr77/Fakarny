38262fa2a0f63c337b04795b5c12846fece6cb96
Ahmed Amr <ahmed.sharf.777@gmail.com>
2021-01-28 21:56:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tripreminder/ui/upcoming/UpcomingViewModel.java
@@ -0,0 +1,19 @@
+package com.example.tripreminder.ui.upcoming;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class UpcomingViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public UpcomingViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Upcoming fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
