42f81657f3d78351a75276b1504d44347b8e2083
Menna <manonaelhelaly@gmail.com>
2021-01-29 15:08:30 +0200

    add button going to next activity--> add New Trip

--- app/src/main/java/com/example/tripreminder/MainActivity.java
+++ app/src/main/java/com/example/tripreminder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tripreminder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -29,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent outIntent =new Intent(getApplicationContext(),NewTripActivity.class);
+                startActivity(outIntent);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
